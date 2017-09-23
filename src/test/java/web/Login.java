07db5debbb6577a19948dd72c26ac130015b29fe
gjain <gaurav.jain1@worldpay.com>
2017-09-23 18:44:01 +0530

    [add before and after tags and checkstyle fixes]

--- src/test/java/web/Login.java
+++ src/test/java/web/Login.java
@@ -1,74 +1,84 @@
 package web;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 
+import utility.Screenshot;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+/**
+ * This class will initialize web driver 
+ * and launch browser along with initial login page navigation.
+ * @author gauravp.jain
+ *
+ */
+
 public class Login {
 	
-	WebDriver webDriver;
+	
 	String webDriverKey;
 	String webDriverPath;
 	DesiredCapabilities capabilities;
 	
-	
-	@Before public void setDriver(){
 
-
-		capabilities = DesiredCapabilities.internetExplorer();
-		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-		capabilities.setCapability(CapabilityType.BROWSER_NAME, "IE");
-		capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
-		capabilities.setCapability("requireWindowFocus", true);
-		capabilities.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
-
-	}
-	
+	/**
+	 * This method will set url of ebay at browser and will open browser at 100% zoom level.
+	 * @param url will contain url for ebay as mentioned in feature file.
+	 */
 	
 	@Given("^We have ebay url as \"([^\"]*)\"$")
 	public void we_have_ebay_url_as(String url)  {
-		webDriverKey="webdriver.ie.driver";
-		webDriverPath="F:\\IEDriver\\IEDriverServer.exe";
+		
+		webDriverKey = "webdriver.ie.driver";
+		webDriverPath = "F:\\IEDriver\\IEDriverServer.exe";
 		System.setProperty(webDriverKey,webDriverPath);
-		webDriver=new InternetExplorerDriver(capabilities);
-		webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		webDriver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
-		webDriver.manage().window().maximize();
-		webDriver.get(url);
+		TestSequence.webDriver = new InternetExplorerDriver(capabilities);
+		TestSequence.webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		TestSequence.webDriver.findElement(
+				By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
+		TestSequence.webDriver.manage().window().maximize();
+		TestSequence.webDriver.get(url);
 	
 		
 	}
+	
+	/**
+	 * This method will click on sign in hyperlink to navigate at sign in page.
+	 */
 	
 	@When("^we click on sign in$")
 	public void we_click_on_sign_in() {
 		
-		WebElement sign=webDriver.findElement(By.xpath("//*[@id='gh-ug']/a"));
+		WebElement sign = TestSequence.webDriver.findElement(
+				By.xpath("//*[@id='gh-ug']/a"));
 		sign.click();
 	 
 	}
 	
+	/**
+	 * This method will verify navigate page is sign in page or not.
+	 * @param expectedTag will contain sign in tag as mentioned in story file.
+	 */
 	
 	@Then("^login page open successfuly as \"([^\"]*)\" tag appears$")
 	public void login_page_open_successfuly_as_tag_appears(String expectedTag) {
 		
-		WebElement signInOption=webDriver.findElement(By.xpath("//*[@id='sgnTab']/span[2]"));
-		String actualTag= signInOption.getText();
+		Screenshot screenshot = new Screenshot();
+		
+		WebElement signInOption = TestSequence.webDriver.findElement(
+				By.xpath("//*[@id='sgnTab']/span[2]"));
+		String actualTag = signInOption.getText();
 
 		Assert.assertEquals(actualTag, expectedTag);
+		screenshot.takeScreenShot("Sign In page");
 		
 	   
 	}
--- src/test/java/web/TestSequence.java
+++ src/test/java/web/TestSequence.java
@@ -1,13 +1,48 @@
 package web;
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"classpath:features/Login.feature"},glue={"web"},format={"pretty"})
+@CucumberOptions(features = {"classpath:features/Login.feature"},glue = {"web"},format = {"pretty"})
 
 public class TestSequence {
+
+	DesiredCapabilities capabilities;
+	public static WebDriver webDriver;
+	
+	/**
+	 * This method will set pre-requisite for IE-driver.
+	 */
+	@Before public void setDriver() {
+		
+	capabilities = DesiredCapabilities.internetExplorer();
+	capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+	capabilities.setCapability(CapabilityType.BROWSER_NAME, "IE");
+	capabilities.setCapability(InternetExplorerDriver
+			.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+	capabilities.setCapability("requireWindowFocus", true);
+	capabilities.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+		
+			}
+	
+	/**
+	 * This method will close browser.
+	 */
+	@After public void closeBrowser() {
+		
+		webDriver.close();
+		
+	}
+	
 
 }
