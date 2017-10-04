fe8ba5d697895b0f8c119c5eda4f1127e3188272
gjain <gaurav.jain1@worldpay.com>
2017-10-04 20:56:03 +0530

    additional feature in helppage

--- src/test/java/web/_3HelpPage.java
+++ src/test/java/web/_3HelpPage.java
@@ -1,10 +1,15 @@
 package web;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import junit.framework.Assert;
 
 import org.openqa.selenium.By;
+
 
 import utility.Screenshot;
 import cucumber.api.DataTable;
@@ -19,71 +24,98 @@ import cucumber.api.java.en.When;
  */
 
 public class _3HelpPage  {
-	
-	
+
+
 	/**
 	 * This method will verify current page.
 	 * @throws InterruptedException will be thrown when a thread 
 	 * is waiting or sleeping and another thread interrupts
 	 */
-	
+
 
 	@Given("^user logged into Ebay$")
 	public void user_logged_into_Ebay() throws InterruptedException  {
-	  
+
 		Thread.sleep(3000);
 		String loginName = TestSequence.webDriver.findElement(
 				By.xpath("//*[@id='gh-ug']")).getText();
-		
+
 		Assert.assertEquals("Hi Gaurav!", loginName);
-		
-	   
+
+
 	}
 
 
-	
+
 	/**
 	 * This method will navigate user to Customer service page.
 	 */
-	
+
 	@When("^user click on Help & Contact link$")
 	public void user_click_on_Help_Contact_link()  {
-	    
+
 		TestSequence.webDriver.findElement(By.xpath("//*[@id='gh-p-3']/a")).click();
-	    
+
 	}
 
-	
+	/**
+	 * This method will verify current page.
+	 * @param currentPage will contain name of current page as mentioned in feature file.
+	 */
+
+	@When("^user navigated to \"([^\"]*)\" page$")
+	public void user_navigated_to_page(String currentPage) {
+
+		String openPage = TestSequence.webDriver.findElement(
+				By.xpath("//*[@id='gh-title']")).getText();
+
+		Assert.assertEquals(openPage, currentPage);
+
+	}
+
+
 	/**
 	 * This method will verify current page is Customer Service or not.
 	 * @param help will contain current page name as mentioned in story file.
 	 * 
 	 */
-	
+
 	@Then("^user will navigate to \"([^\"]*)\" page$")
 	public void user_will_navigate_to(String help) {
 		String pageTitle = TestSequence.webDriver.findElement(
 				By.xpath("//*[@id='gh-title']")).getText();
-		
+
 		Assert.assertEquals(pageTitle, help);
 		Screenshot screenShot = new Screenshot();
 		screenShot.takeScreenShot("Customer Service");
-	    
-	}
-	
-	@When("^user navigated to Customer Service page$")
-	public void user_navigated_to_Customer_Service_page()  {
-	    // Write code here that turns the phrase above into concrete actions
-	    
+
 	}
 
-	@Then("^user will find below catagory for help$")
-	public void user_will_find_below_catagory_for_help(DataTable helpOptions) {
-	  List<List<String>> readRecord= helpOptions.raw();
-	  for(int i=1;i<readRecord.size();i++) {
-	  System.out.println("RECORD::"+readRecord.get(i).get(0));
-	  }
-	  
+	/**
+	 * This method will verify help page options.
+	 * @param helpOptions will contains options as mentioned in feature file.
+	 */
+
+	@Then("^user will find below catagory for help \"([^\"]*)\"$")
+	public void user_will_find_below_catagory_for_help(String helpOptions)  {
+
+
+		String solution = TestSequence.webDriver.findElement(
+				By.xpath("//*[@id='body']/div[2]/div[4]/div[2]/h2")).getText();
+		String topic = TestSequence.webDriver.findElement(
+				By.xpath(" //*[@id='browseByTopic']")).getText();
+
+		Set<String> helpTopic = new TreeSet<String>();
+		helpTopic.add(helpOptions);	
+
+		Iterator it = helpTopic.iterator();
+		while (it.hasNext()) {
+			it.next();
+			helpTopic.contains(solution);
+			helpTopic.contains(topic);
+		}
+
 	}
+
 
 }
--- src/test/resources/features/_3HelpPage.feature
+++ src/test/resources/features/_3HelpPage.feature
@@ -4,10 +4,11 @@ Given user logged into Ebay
 When user click on Help & Contact link
 Then user will navigate to "Customer Service" page
 
-Scenario: Catagories available at Customer Service page
+Scenario Outline: Catagories available at Customer Service page
 Given user logged into Ebay
-When user navigated to Customer Service page
-Then user will find below catagory for help
+When user navigated to "Customer Service" page
+Then user will find below catagory for help "<Options>"
+Examples:
 |Options|
 |Popular solutions|
 |Browse by topic|
